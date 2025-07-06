c28774df7dce7c89cc150aa66a18b540f2a93779
Lukas <lukas.hofbauer@student.uibk.ac.at>
2025-07-06 11:20:07 +0900

    commit changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='terminal_gpt',
     version='0.1',
-    py_modules=['main'],
+    py_modules=['main', 'chat'],
     install_requires=[
         'litellm',
         'urwid',
